291d9cd38f51ca4893a69d1f7c1517fe3d33705f
danielin0526 <danielarpe05@gmail.com>
2024-12-05 22:05:34 +0100

    BuscaTesorosHecho

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises
 const readline = require('readline').promises;
 
 let matriu = [
@@ -29,6 +30,78 @@ const filaIndices = {
     "F": 6
 };
 
+const salto = "\n";
+
+const menu =
+    "------ MENU ------" + salto +
+    "1: Ajuda" + salto +
+    "2: Carregar Partida" + salto +
+    "3: Guardar Partida" + salto +
+    "4: Trampa" + salto +
+    "5: Destapar" + salto +
+    "6: Puntuacio" + salto +
+    "0: Sortir" + salto;
+
+const menuAjuda =
+    "------ MENU AJUDA ------" + salto +
+    "Les opcions: 1, ajuda, help. ----> Son per mostrar aquest menu d'ajuda" + salto +
+    "Les opcions: 2, carregar partida. ----> Carregaran la partida del arxiu que seleccionis." + salto +
+    "Les opcions: 3, guardar partida. ----> Guardaran la partida per poder carregarla mes tard" + salto +
+    "Les opcions: 4, trampa, cheat. ----> Mostraran el taulell amb les caselles que tenen tressors destapades" + salto +
+    "Les opcions: 5, destapar. ----> Serviran per destapar una casella escribint x + y per exemple 'D7'" + salto +
+    "Les opcions: 6, puntuacio. ----> Serveixen per veure la puntuacio y les tirades restants" + salto +
+    "Les opcions: 0, sortir, exit. ----> Serveixen per sortir del joc. SI NO HAS GUARDAT AVANTS EL PROGRES ES PERDRA" + salto;
+
+let tresorsRestants = 16
+let tiradas = 0
+let maxTiradas = 32
+let trampas = false
+
+// Funció per escriure un objecte en un arxiu .json
+async function guardarPartida(obj, file_path) {
+    try {
+        const txtData = JSON.stringify(obj, null, 2)
+        await fs.writeFile(file_path, txtData, 'utf-8')
+    } catch (error) {
+        console.error("Error en escriure les dades:", error)
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function carregarPartida(file_path) {
+    try {
+        const txtData = await fs.readFile(file_path, 'utf-8')
+        const data = JSON.parse(txtData)
+        
+        // Actualizar las variables globales con los valores cargados
+        matriu = data.taulell;
+        matriuTresors = data.taulellTresors;
+        tresorsRestants = data.tresorsRestants;
+        tiradas = data.tiradas;
+
+        console.log("Carregant partida: temps restant 3 segons")
+        for (let segons = 3; segons != 0; segons--){
+            console.log(segons)
+            await sleep(1000)
+        }
+        console.log("Press Enter to continue")
+
+    } catch (error) {
+        console.error("Error en llegir les dades:", error)
+    }
+}
+
+function mostraPuntuacio(){
+    let punts = 16 - tresorsRestants 
+    console.log("---------- PUNTUACIO ----------")
+    console.log(`Punts: ${punts}/16`)
+    console.log(`Tirades: ${tiradas}/${maxTiradas}`)
+    console.log("-------------------------------") 
+}
+
 function enterraTresors(){
     for (let i = 0; i < 16; i++){
         fila = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
@@ -42,16 +115,47 @@ function enterraTresors(){
     }
 }
 
-function printaTauler() {
-    for (let fila of matriu) {
-        console.log(fila.join(" "));
+function distanciaTesoroMasCercano(filaIndex, columna) {
+    let menorDistancia = 1000
+    let hayTesoro = false
+
+    for (let fila = 1; fila < matriuTresors.length; fila++) { // Recorre filas
+        for (let col = 1; col < matriuTresors[fila].length; col++) { // Recorre columnass
+            if (matriuTresors[fila][col] === "#" && matriu[fila][col] !== "O") { // Si hay un tesoro
+                hayTesoro = true
+                let distancia = Math.abs(filaIndex - fila) + Math.abs(columna - col)
+                if (distancia < menorDistancia) {
+                    menorDistancia = distancia
+                }
+            }
+        }
     }
+
+    return menorDistancia // Si no hay tesoros, devolvemos -1
 }
 
-function printaTaulerTresors() {
-    for (let fila of matriuTresors) {
-        console.log(fila.join(" "));
+function printaTauler() {
+    if(trampas){
+        printaTaulers()
+    }else{
+        console.log(salto + "------- TAULELL -------" + salto)
+        for (let fila of matriu) {
+            console.log(fila.join(" "));
+        }
     }
+    console.log()
+    
+}
+
+function printaTaulers() {
+    console.log(salto + "------- TAULELL -------" + "           " + "------- TAULELL DESTAPAT -------" + salto);
+    
+    for (let i = 0; i < matriu.length; i++) {
+        let filaMatriu = matriu[i].join(" ");           // Convierte la fila del primer tablero a string
+        let filaTresors = matriuTresors[i].join(" ");   // Convierte la fila del segundo tablero a string
+        console.log("  " + filaMatriu + "                      " + filaTresors); // Une ambas filas con un espacio entre ellas
+    }
+    console.log();
 }
 
 async function main() {
@@ -60,33 +164,12 @@ async function main() {
         output: process.stdout
     });
 
-    const salto = "\n";
-    const menu =
-        "------ MENU ------" + salto +
-        "1: Ajuda" + salto +
-        "2: Carregar Partida" + salto +
-        "3: Guardar Partida" + salto +
-        "4: Trampa" + salto +
-        "5: Destapar" + salto +
-        "6: Puntuacio" + salto +
-        "0: Sortir" + salto;
-
-    const menuAjuda =
-        "------ MENU AJUDA ------" + salto +
-        "Les opcions: 1, ajuda, help. ----> Son per mostrar aquest menu d'ajuda" + salto +
-        "Les opcions: 2, carregar partida. ----> Carregaran la partida del arxiu que seleccionis." + salto +
-        "Les opcions: 3, guardar partida. ----> Guardaran la partida per poder carregarla mes tard" + salto +
-        "Les opcions: 4, trampa, cheat. ----> Mostraran el taulell amb les caselles que tenen tressors destapades" + salto +
-        "Les opcions: 5, destapar. ----> Serviran per destapar una casella escribint x + y per exemple 'D7'" + salto +
-        "Les opcions: 6, puntuacio. ----> Serveixen per veure la puntuacio y les tirades restants" + salto +
-        "Les opcions: 0, sortir, exit. ----> Serveixen per sortir del joc. SI NO HAS GUARDAT AVANTS EL PROGRES ES PERDRA" + salto;
-
     enterraTresors()
     printaTauler();
     console.log(menu);
 
     let exit = false;
-    while (!exit) {
+    while (!exit && tiradas !== maxTiradas && tresorsRestants !== 0) {
         let opcion = (await rl.question("Opcio: ")).trim().toLowerCase();
 
         switch (opcion) {
@@ -98,35 +181,75 @@ async function main() {
                 break;
             case "2":
             case "carregar partida":
-                console.log("Función para cargar partida no implementada.");
+                const rutaCarrega = await rl.question("Nom de l'arxiu a carregar? ")
+
+                carregarPartida(rutaCarrega)
+                await rl.question(); 
                 break;
             case "3":
             case "guardar partida":
-                console.log("Función para guardar partida no implementada.");
+                const ruta = await rl.question("Nom de l'arxiu a generar? ")
+
+                const dades = {
+                    taulell: matriu,
+                    taulellTresors: matriuTresors,
+                    tresorsRestants: tresorsRestants,
+                    tiradas: tiradas
+                }
+
+                guardarPartida(dades, ruta)
+                console.log(salto + `Dades escrites a ${ruta}`)
+
                 break;
             case "4":
             case "trampa":
             case "cheat":
-                console.log(salto + "------- TAULELL DESTAPAT -------" + salto)
-                printaTaulerTresors()
+                if (!trampas){
+                    console.log("Trampes activades, a partir d'hara es mostrara el taulell resolt")
+                    trampas = true
+                }else{
+                    console.log("Trampes desactivades, ja no es mostrara el taulell resolt")
+                    trampas = false    
+                }
                 break;
             case "5":
-                case "destapar":
-                    let coordenada = (await rl.question("Introduce coordenada (ejemplo: D7): ")).trim().toUpperCase()
-                    let fila = coordenada[0] // Primera letra (ejemplo: 'D')
-                    let columna = parseInt(coordenada[1]) // Resto como número (ejemplo: '7')
-    
-                    if (fila in filaIndices && columna >= 1 && columna <= 7) {
-                        let filaIndex = filaIndices[fila] // Obtenemos el índice numérico de la fila
-                        matriu[filaIndex][columna] = "B" // Marcamos la casilla
+            case "destapar":
+                let coordenada = (await rl.question("Introduce coordenada (ejemplo: D7): ")).trim().toUpperCase();
+            
+                let fila = coordenada[0]; // Primera letra (ejemplo: 'D')
+                let columna = parseInt(coordenada[1]) + 1; // Resto como número (ejemplo: '7')
+            
+                if (fila in filaIndices && columna >= 1 && columna <= 8 && coordenada.length <= 2) {
+                    let filaIndex = filaIndices[fila]; // Obtenemos el índice numérico de la fila
+                    if (matriuTresors[filaIndex][columna] === "#") {
+                        matriu[filaIndex][columna] = "O"; // Marcamos la casilla
                         console.log(`Casella ${coordenada} destapada.`);
+                        console.log(`Enhorabona, has destapat un tresor!`);
+                        tresorsRestants--
                     } else {
-                        console.log("Coordenada inválida.")
+                        matriu[filaIndex][columna] = " "; // Marcamos la casilla
+                        console.log(`Casella ${coordenada} destapada.`);
+                        console.log(`No hi habia res a aquesta casella!`);
+                        let distancia = distanciaTesoroMasCercano(filaIndex, columna);
+                        console.log(`El tesoro más cercano está a ${distancia} casilla(s).`);
+                        tiradas++
+                        
                     }
-                    break;
+                } else {
+                    console.log("Coordenada inválida.");
+                }
+
+                if (tiradas === maxTiradas){
+                    console.log(salto + `T'has quedat sense tirades! Has perdut, queden ${tresorsRestants} tresors restants.` + salto)
+                }else if (tresorsRestants === 0){
+                    console.log(salto + `Has destapat tots els tresors! Has guanyat amb nomes ${tiradas} tirades.` + salto)
+                }
+
+                break;
             case "6":
             case "puntuacio":
-                console.log("Función para mostrar puntuación no implementada.");
+                mostraPuntuacio()
+                await rl.question("Press Enter to continue");
                 break;
             case "0":
             case "sortir":
@@ -138,9 +261,10 @@ async function main() {
             default:
                 console.log("¡Opción inválida!");
         }
-
-        printaTauler();
-        console.log(menu);
+        if(!exit && tiradas !== maxTiradas && tresorsRestants !== 0){
+            printaTauler();
+            console.log(menu);
+        }
     }
     rl.close();
 }
